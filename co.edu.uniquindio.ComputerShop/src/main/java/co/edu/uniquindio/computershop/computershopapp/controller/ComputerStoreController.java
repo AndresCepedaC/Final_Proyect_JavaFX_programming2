7e79fc5f387cff54f1d7813fb09539d053f03922
AndresCepedaC <143088681+AndresCepedaC@users.noreply.github.com>
2024-03-21 12:25:28 -0500

    controller mouse semicompleto

--- co.edu.uniquindio.ComputerShop/src/main/java/co/edu/uniquindio/computershop/computershopapp/controller/ComputerStoreController.java
+++ co.edu.uniquindio.ComputerShop/src/main/java/co/edu/uniquindio/computershop/computershopapp/controller/ComputerStoreController.java
@@ -1,4 +1,21 @@
 package co.edu.uniquindio.computershop.computershopapp.controller;
 
+import co.edu.uniquindio.computershop.computershopapp.modelfactory.ModelFactory;
+import javafx.fxml.FXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class ComputerStoreController {
+    ModelFactory modelFactory;
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+        modelFactory = ModelFactory.getInstancia();
+    }
 }
--- co.edu.uniquindio.ComputerShop/src/main/java/co/edu/uniquindio/computershop/computershopapp/controller/MouseController.java
+++ co.edu.uniquindio.ComputerShop/src/main/java/co/edu/uniquindio/computershop/computershopapp/controller/MouseController.java
@@ -1,4 +1,14 @@
 package co.edu.uniquindio.computershop.computershopapp.controller;
 
+import co.edu.uniquindio.computershop.computershopapp.model.ComputerStore;
+import co.edu.uniquindio.computershop.computershopapp.model.Mouse;
+import co.edu.uniquindio.computershop.computershopapp.modelfactory.ModelFactory;
+
+import java.util.List;
+
 public class MouseController {
+    ModelFactory modelFactory;
+    public List<Mouse> obtenerMouses() {
+        return  modelFactory.obtenerMouses();
+    }
 }
